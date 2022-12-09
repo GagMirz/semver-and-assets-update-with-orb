eb1d79eb69ad8ed4a21cd40b5ce49b3bd6c48b45
GagMirz <gmirzoyan12@gmail.com>
2022-12-09 15:51:31 +0400

    major

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -4,7 +4,7 @@
 
 option="<< parameters.level >>"
 # shellcheck disable=SC2086
-version=$(curl https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/latest -s | jq .name -r)
+version="<< parameters.version >>"
 
 while read -r -n1 op; do
   case $op in
