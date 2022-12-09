08edee10d2f2fd5f9facef919f9b139e2274ebb0
GagMirz <gmirzoyan12@gmail.com>
2022-12-09 17:03:39 +0400

    filter

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo 1
 # Increment a version string using Semantic Versioning terminology.
-echo 2
 
 # shellcheck disable=SC2154
 while read -r -n1 op; do
@@ -11,11 +9,9 @@ while read -r -n1 op; do
     p ) patch=true;;
   esac
 done < <(echo -n "$option")
-echo 3
 
 # shellcheck disable=SC2206 
 a=( ${version//./ } )
-echo 4
 
 # shellcheck disable=SC2236
 if [ ! -z $major ]; then
@@ -27,23 +23,14 @@ fi
 
 # shellcheck disable=SC2236
 if [ ! -z $minor ]; then
-  echo ${a[0]}
-  echo ${a[1]}
-  echo ${a[2]}
-  echo 4.1
   ((a[1]++))
-  echo 4.2
   a[2]=0
-  echo 4.3
 fi
-echo 6
 
 # shellcheck disable=SC2236
 if [ ! -z $patch ]; then
   ((a[2]++))
 fi
-echo 7
 
 version="${vFlag}${a[0]}.${a[1]}.${a[2]}"
 echo "export VERSION=${version}" >> "$BASH_ENV"
-echo $version
