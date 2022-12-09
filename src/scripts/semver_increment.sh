798d4df4dc65718c97fd1ca63ac94a9ffe4486ee
GagMirz <gmirzoyan12@gmail.com>
2022-12-09 14:44:43 +0400

    patch

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -22,19 +22,19 @@ fi
 a=( ${version//./ } )
 
 # Increment version numbers as requested.
-if [ ! -z $major ]; then
+if [ -n $major ]; then
   vFlag="v"
   ((a[0]++))
   a[1]=0
   a[2]=0
 fi
 
-if [ ! -z $minor ]; then
+if [ -n $minor ]; then
   ((a[1]++))
   a[2]=0
 fi
 
-if [ ! -z $patch ]; then
+if [ -n $patch ]; then
   ((a[2]++))
 fi
 
