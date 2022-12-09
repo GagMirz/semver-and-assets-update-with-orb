3e25df80e0a35e1b615c724fa958efa5f1d8500c
GagMirz <gmirzoyan12@gmail.com>
2022-12-09 15:12:16 +0400

    minor

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -3,7 +3,7 @@
 # Increment a version string using Semantic Versioning terminology.
 
 option="<< parameters.level >>"
-version=`curl https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases/latest -s | jq .name -r`
+version=$(curl https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases/latest -s | jq .name -r)
 
 while read -n1 op; do
   case $op in
