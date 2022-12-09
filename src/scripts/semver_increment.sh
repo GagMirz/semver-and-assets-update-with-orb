e8d6157b75d06f9531fdee82f431816339078c2b
GagMirz <gmirzoyan12@gmail.com>
2022-12-09 15:14:04 +0400

    fuck your shellcheck

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -3,6 +3,7 @@
 # Increment a version string using Semantic Versioning terminology.
 
 option="<< parameters.level >>"
+# shellcheck disable=SC2086
 version=$(curl https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/latest -s | jq .name -r)
 
 while read -n1 op; do
