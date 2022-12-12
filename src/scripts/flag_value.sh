769a40d0f133c3b0823259c469cdc703661cfb00
GagMirz <gmirzoyan12@gmail.com>
2022-12-12 17:49:33 +0400

    ff

--- src/scripts/flag_value.sh
+++ src/scripts/flag_value.sh
@@ -3,10 +3,10 @@
 export PYTHONIOENCODING=utf8
 # shellcheck disable=SC2154
 PULL_REQUEST_BASE_REF=$(python3 -c "import sys, json, re; \
-text=\"${text}\" \
+text=\"${text}\"; \
 matches=re.findall( \
 '(?<=[-{1,2}|\/])(?P<name>[a-zA-Z0-9]*)[ |:|\"]*(?P<value>[\w|.|?|=|&|+| |:|\/|\\]*)(?=[ |\"]|$)', \
-title); \
+text); \
 value=[argument for argument in matches if argument[0] == \"${flag}\"][0][1]; \
 print(value)")
 # shellcheck disable=SC2154
