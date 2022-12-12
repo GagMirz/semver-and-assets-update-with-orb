55c122d8c465a9cada3a0a22b12b26caac8ea16d
GagMirz <gmirzoyan12@gmail.com>
2022-12-12 19:06:43 +0400

    asdf

--- src/scripts/flag_value.sh
+++ src/scripts/flag_value.sh
@@ -11,7 +11,7 @@ echo "${answer}"
 PULL_REQUEST_BASE_REF=$(python3 -c "import sys, json, re; \
 text=\"${text}\"; \
 matches=re.findall( \
-'(?<=[-{1,2}|\/])(?P<name>[a-zA-Z0-9]*)[ |:|\"]*(?P<value>[\w|.|?|=|&|+| |:|\/|\\]*)(?=[ |\"]|$)', \
+r'(?<=[-{1,2}|\/])(?P<name>[a-zA-Z0-9]*)[ |:|\\\"]*(?P<value>[\w]*)(?=[ |\\\"]|$)', \
 text); \
 value=[argument for argument in matches if argument[0] == \"${flag}\"][0][1]; \
 print(value)")
