077c5299edadf976040e96d59eb7417c974f4894
GagMirz <gmirzoyan12@gmail.com>
2022-12-12 17:54:18 +0400

    te

--- src/scripts/flag_value.sh
+++ src/scripts/flag_value.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 export PYTHONIOENCODING=utf8
+echo $text
+echo $flag
+echo $answer
 # shellcheck disable=SC2154
 PULL_REQUEST_BASE_REF=$(python3 -c "import sys, json, re; \
 text=\"${text}\"; \
-print(text); \
 matches=re.findall( \
 '(?<=[-{1,2}|\/])(?P<name>[a-zA-Z0-9]*)[ |:|\"]*(?P<value>[\w|.|?|=|&|+| |:|\/|\\]*)(?=[ |\"]|$)', \
 text); \
