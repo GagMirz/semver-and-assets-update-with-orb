f4a887142e60bea0199f698f116b420b70764484
GagMirz <gmirzoyan12@gmail.com>
2022-12-12 17:45:26 +0400

    try

--- src/scripts/flag_value.sh
+++ src/scripts/flag_value.sh
@@ -2,14 +2,12 @@
 
 export PYTHONIOENCODING=utf8
 # shellcheck disable=SC2154
-PULL_REQUEST_BASE_REF=$(python3 -c "import sys, json, re; \
+PUxL_REQUEST_BASE_REF=$(python3 -c "import sys, json, re; \
 text=\"${text}\" \
 matches=re.findall( \
 '(?<=[-{1,2}|\/])(?P<name>[a-zA-Z0-9]*)[ |:|\"]*(?P<value>[\w|.|?|=|&|+| |:|\/|\\]*)(?=[ |\"]|$)', \
 title); \
 value=[argument for argument in matches if argument[0] == \"${flag}\"][0][1]; \
-print(value)"
-${text})
-print()
+print(value)")
 # shellcheck disable=SC2154
 echo "export ${answer}=${PULL_REQUEST_BASE_REF}" >> "$BASH_ENV"
