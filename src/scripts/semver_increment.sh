36c72d97c70e96cb38e2024cffe8da61aad6f10a
GagMirz <gmirzoyan12@gmail.com>
2022-12-13 16:55:01 +0400

    asdf

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -5,7 +5,7 @@
 # SC2206,SC2236 justification: Meaningless warning/error. 
 # shellcheck disable=all
 
-source $cnfp
+[[ -f $cnfp ]] && source $cnfp
 
 # Add default values
 [[ -z $version ]] && version="v0.0.0"
