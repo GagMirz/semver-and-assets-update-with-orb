6d44eebe5900ffd7538723b438b0c5dea30bb63e
GagMirz <gmirzoyan12@gmail.com>
2022-12-13 20:41:25 +0400

    updated descriptions

--- src/scripts/commit_message.sh
+++ src/scripts/commit_message.sh
@@ -2,11 +2,14 @@
 # shellcheck disable=SC2154
 # SC2154 justification: Variable assigned outside of script file.
 # shellcheck disable=all
+echo $commit_hash
 
 [[ -f $cnfp ]] && source $cnfp
-
+echo $commit_hash
 # Add default values
 [[ -z $commit_hash ]] && commit_hash=$CIRCLE_SHA1
+echo $commit_hash
+
 [[ -z $answer ]] && answer="COMMIT_MESSAGE"
 
 message=$(git log --format=oneline -n 1 $CIRCLE_SHA1)
