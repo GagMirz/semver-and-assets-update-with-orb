7c29539e06df47e843c865196d663d5ab3509126
GagMirz <gmirzoyan12@gmail.com>
2022-12-13 16:40:50 +0400

    updated tests

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -5,6 +5,8 @@
 # SC2206,SC2236 justification: Meaningless warning/error. 
 # shellcheck disable=all
 
+source $config_path
+
 # Add default values
 [[ -z $version ]] && version="v0.0.0"
 [[ -z $option ]] && option="p"
