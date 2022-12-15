d5366b7baf63a2409e945abd9edc84749474f91e
GagMirz <gmirzoyan12@gmail.com>
2022-12-15 18:57:20 +0400

    fstan

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shellcheck disable=SC2154,SC2236,SC1090
-# SC2206,SC2236 justification: Meaningless warning/error.
+# SC2236 justification: Meaningless warning/error.
 # SC1090 justification: file should be created outside, path is not fixed, can't specify source.
 # SC2154 justification: Variable assigned outside of script file(Depends on SC1090).
 
@@ -17,6 +17,7 @@ semver_increment() {
   local a
   local vFlag
 
+  # shellcheck disable=SC2206 # Justification: Need it to be splited to array
   a=(${1//./ })
 
   if [ "${a[0]:0:1}" == "v" ]; then
