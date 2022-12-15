fa29f9f64afdf2dbcb10b7b9584ee1867f65cf4d
GagMirz <gmirzoyan12@gmail.com>
2022-12-15 18:55:38 +0400

    test

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# shellcheck disable=SC2154,SC2206,SC2236,SC1090
+# shellcheck disable=SC2154,SC2236,SC1090
 # SC2206,SC2236 justification: Meaningless warning/error.
 # SC1090 justification: file should be created outside, path is not fixed, can't specify source.
 # SC2154 justification: Variable assigned outside of script file(Depends on SC1090).
