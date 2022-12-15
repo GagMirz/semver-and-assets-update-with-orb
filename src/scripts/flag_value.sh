bba474e1f383c074493198974b1be36c871d3c32
GagMirz <gmirzoyan12@gmail.com>
2022-12-15 17:04:29 +0400

    test

--- src/scripts/flag_value.sh
+++ src/scripts/flag_value.sh
@@ -7,20 +7,25 @@
 source src/scripts/utils.sh
 SourceParameters required
 
-# Add default values
-[[ -z "${text}" ]] && exit 128
-[[ -z "${flag}" ]] && exit 128
-[[ -z "${answer}" ]] && answer="FLAG_VALUE"
+get_flag_value() {
+    # Add default values
+    [[ -z "${text}" ]] && exit 128
+    [[ -z "${flag}" ]] && exit 128
+    [[ -z "${answer}" ]] && answer="FLAG_VALUE"
 
-export PYTHONIOENCODING=utf8
+    export PYTHONIOENCODING=utf8
 
-FLAG_VALUE=$(python3 -c "import re; \
-text = \"${text}\"; \
-matches = re.findall( \
-    r'(?<=[-{1,2}|\/])(?P<name>[a-zA-Z0-9]*)[ |:|\\\"]*(?P<value>[\w]*)(?=[ |\\\"]|$)', \
-    text); \
-matches = [argument for argument in matches if argument[0] == \"${flag}\"];
-value = matches[0][1] if len(matches) else \"\"; \
-print(value)")
+    local FLAG_VALUE
+    FLAG_VALUE=$(python3 -c "import re; \
+    text = \"${text}\"; \
+    matches = re.findall( \
+        r'(?<=[-{1,2}|\/])(?P<name>[a-zA-Z0-9]*)[ |:|\\\"]*(?P<value>[\w]*)(?=[ |\\\"]|$)', \
+        text); \
+    matches = [argument for argument in matches if argument[0] == \"${flag}\"];
+    value = matches[0][1] if len(matches) else \"\"; \
+    print(value)")
 
-CreateAnswer "${answer}" "${FLAG_VALUE}"
+    echo FLAG_VALUE
+}
+
+CreateAnswer "${answer}" "$(get_flag_value)"
