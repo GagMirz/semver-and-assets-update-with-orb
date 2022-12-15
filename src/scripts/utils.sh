d3b5a79f64e0cec5999f6711d3d2afc3d535a2e7
GagMirz <gmirzoyan12@gmail.com>
2022-12-15 13:45:09 +0400

    try

--- src/scripts/utils.sh
+++ src/scripts/utils.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SC2206,SC2236 justification: Meaningless warning/error. 
+# SC1090 justification: file should be created outside, path is not fixed, can't specify source.
+# SC2154 justification: Variable assigned outside of script file(Depends on SC1090).
 
 SourceParameters() {
     [[ -f "${cnfp}" ]] && source "${cnfp}" || [[ "${1}" == "required" ]] && exit 127
