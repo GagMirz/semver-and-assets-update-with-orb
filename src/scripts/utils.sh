2390c0a87b45492a18f7ed9eea8d59b642c568e8
GagMirz <gmirzoyan12@gmail.com>
2022-12-15 14:27:42 +0400

    asdf

--- src/scripts/utils.sh
+++ src/scripts/utils.sh
@@ -4,5 +4,7 @@
 # SC2154 justification: Variable assigned outside of script file(Depends on SC1090).
 
 SourceParameters() {
+    echo "${cnfp}"
+    [[ -f "${cnfp}"]] && echo "exists"
     [[ -f "${cnfp}" ]] && source "${cnfp}" || [[ "${1}" == "required" ]] && exit 127
 }
