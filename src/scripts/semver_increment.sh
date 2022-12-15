9071f307dd760b51eeb7f113c1abb1cef57cc280
GagMirz <gmirzoyan12@gmail.com>
2022-12-15 18:05:35 +0400

    fstx

--- src/scripts/semver_increment.sh
+++ src/scripts/semver_increment.sh
@@ -14,35 +14,41 @@ SourceParameters
 [[ -z "${option}" ]] && option="p"
 [[ -z "${answer}" ]] && answer="VERSION"
 
-while read -r -n1 op; do
-  case $op in
-  M) major=true ;;
-  m) minor=true ;;
-  p) patch=true ;;
-  esac
-done < <(echo -n "$option")
+semver_increment() {
+  while read -r -n1 op; do
+    case $op in
+    M) major=true ;;
+    m) minor=true ;;
+    p) patch=true ;;
+    esac
+  done < <(echo -n "$option")
 
-a=(${version//./ })
+  a=(${version//./ })
 
-if [ "${a[0]:0:1}" == "v" ]; then
-  a[0]="${a[0]:1}"
-  vFlag="v"
-fi
+  if [ "${a[0]:0:1}" == "v" ]; then
+    a[0]="${a[0]:1}"
+    vFlag="v"
+  fi
 
-if [ ! -z "${major}" ]; then
-  ((a[0] = a[0] + 1))
-  a[1]=0
-  a[2]=0
-fi
+  if [ ! -z "${major}" ]; then
+    ((a[0] = a[0] + 1))
+    a[1]=0
+    a[2]=0
+  fi
 
-if [ ! -z "${minor}" ]; then
-  ((a[1] = a[1] + 1))
-  a[2]=0
-fi
+  if [ ! -z "${minor}" ]; then
+    ((a[1] = a[1] + 1))
+    a[2]=0
+  fi
 
-if [ ! -z "${patch}" ]; then
-  ((a[2] = a[2] + 1))
-fi
+  if [ ! -z "${patch}" ]; then
+    ((a[2] = a[2] + 1))
+  fi
 
-version="${vFlag}${a[0]}.${a[1]}.${a[2]}"
-CreateAnswer "${answer}" "${version}"
+  local version
+  version="${vFlag}${a[0]}.${a[1]}.${a[2]}"
+
+  echo "${version}"
+}
+
+CreateAnswer "${answer}" "$(semver_increment)"
