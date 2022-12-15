b2f0c9f926498d372984cd63cb2a9a8f49bad2d6
GagMirz <gmirzoyan12@gmail.com>
2022-12-15 14:29:07 +0400

    mfix

--- src/scripts/utils.sh
+++ src/scripts/utils.sh
@@ -5,6 +5,6 @@
 
 SourceParameters() {
     echo "${cnfp}"
-    [[ -f "${cnfp}"]] && echo "exists"
+    [[ -f "${cnfp}" ]] && echo "exists"
     [[ -f "${cnfp}" ]] && source "${cnfp}" || [[ "${1}" == "required" ]] && exit 127
 }
