a89d6c8b0860dfcfb7e6b1f0167650e8fa99250c
GagMirz <gmirzoyan12@gmail.com>
2022-12-16 13:30:16 +0400

    documented

--- src/scripts/utils.sh
+++ src/scripts/utils.sh
@@ -3,7 +3,16 @@
 # SC1090 justification: file should be created outside, path is not fixed, can't specify source.
 # SC2154 justification: Variable assigned outside of script file(Depends on SC1090).
 
-# 1 is configuration file required or not
+#######################################
+# Source parameters file into bash
+# ARGUMENTS:
+#   Path to configuration file
+#   Is file required or no
+# RETURN:
+#   0 if print succeeds
+#   127 if file is required and missing
+#   non-zero on other errors
+#######################################
 SourceParameters() {
     if [[ -f "${1}" ]]; then
         source "${1}"
@@ -12,10 +21,22 @@ SourceParameters() {
             exit 127
         fi
     fi
+
+    exit 0
 }
 
-# 1 variable name
-# 2 variable value
+#######################################
+# Create answer variable and insert into shared file 
+# GLOBALS:
+#   BASH_ENV
+# ARGUMENTS:
+#   Variable name
+#   Variable value
+# RETURN:
+#   0 if print succeeds, non-zero on error.
+#######################################
 CreateAnswer() {
     echo "export $1=\"$2\"" >>"$BASH_ENV"
+
+    exit 0
 }
